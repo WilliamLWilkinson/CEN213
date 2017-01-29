0a866f9ccb4042cf08411056355e23ab60eb38e7
WilliamLWilkinson <wwilkinson@albany.edu>
2017-01-29 15:29:50 -0500

    Added lines of code to print "Hello world" to console.

--- src/code/HelloWorld.java
+++ src/code/HelloWorld.java
@@ -1,10 +1,18 @@
+/*
+  	Date: 1/29/2017
+  	Author: William Wilkinson
+	Assignment one: The purpose is to print hello world
+	
+*/
 package code;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String message = new String();	// String variable to hold message to be printed to screen
+		message = "Hello world!";	// Variable message gets assigned "Hello World:
+		System.out.println(message);	// System prints message to console
 	}
 
 }
